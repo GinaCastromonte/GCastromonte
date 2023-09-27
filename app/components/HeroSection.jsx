20a58015e24db6e92b2bcfe85969842e96a2e829
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2023-09-27 17:55:32 -0400

    finished

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,13 +1,19 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
+        < motion.div
+          initial={{ opacity: 0 , scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition = {{duration: 0.5 }}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
           <h1 className="text-white mb-4 text-4xl  sm:text-5lg lg:text-6xl font-extrabold">
             <span className=" text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600 ">
               Hello, I'm{" "}
@@ -28,7 +34,9 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Discover Digital Wonders: Welcome to My Interactive Web Journey
+            Welcome to my portfolio site, where you can explore my journey as a
+            Software Engineer and Developer, showcasing my proficiency in a wide
+            range of web development technologies.
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-600 via-blue-400 to-blue-300 hover:bg-slate-200 text-white">
@@ -40,18 +48,21 @@ const HeroSection = () => {
               </span>
             </button>
           </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4  lg:mt-0">
+        </motion.div>
+        <motion.div 
+          initial={{ opacity: 0 , scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition = {{duration: 0.5 }} className="col-span-5 place-self-center mt-4  lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/gina.png"
-              alt="hero image"
+              alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-[65%] sm:top-1/2 left-1/2"
               width={300}
               height={300}
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState ,useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
+import {motion, useInView} from "framer-motion"
 
 const projectsData = [
   {
@@ -49,6 +50,9 @@ const projectsData = [
 
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
+
+  const ref = useRef(null)
+  const isInView = useInView(ref, {once:true})
   const handleTagChange = (newTag) => {
     setTag(newTag);
   };
@@ -57,8 +61,13 @@ const ProjectsSection = () => {
     project.tag.includes(tag)
   )
 
+  const cardVariants = {
+    initial:{y:50,opacity:0},
+    animate: {y:0 , opacity:1}
+  }
+
   return (
-    <div id="projects" >
+    <div id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
@@ -74,18 +83,26 @@ const ProjectsSection = () => {
           isSelected={tag === "Web"}
         />
       </div>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            description={project.description}
-            imgUrl={project.image}
-            gitUrl={project.gitUrl}
-            previewUrl={project.previewUrl}
-          />
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition = {{duration: 0.3, delay:index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
         ))}{" "}
-      </div>
+      </ul>
     </div>
   );
 };
