f5c3a9f4c355d1e1bd5bc04cc866f6d1cd2a26b8
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2023-09-27 13:12:12 -0400

    working on email

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+"use client";
 
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
@@ -45,28 +47,47 @@ const projectsData = [
   },
 ];
 
-
-
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  )
+
   return (
     <>
-      <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>My Projects</h2>
-      <div>
-        {" "}
-        {projectsData.map((project) => (
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6 ">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
-            gitUrl= {project.gitUrl}
+            gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}
-
           />
         ))}{" "}
       </div>
     </>
   );
-}
+};
 
-export default ProjectsSection
+export default ProjectsSection;
