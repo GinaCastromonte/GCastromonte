4a9de07461e41275880b1da2cc80c6219706816b
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2023-09-27 20:47:49 -0400

    fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,8 @@ import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
 import { Bars3Icon} from "@heroicons/react/24/solid";
-import XmarkIcon from '@heroicons/react/outline/XmarkIcon'; 
+import XmarkIcon from "@heroicons/react/solid/XIcon";
+
 
 
 
